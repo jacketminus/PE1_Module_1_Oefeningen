eedd9ee34e418956c61339b0e9e5c1ca838b08ef
jacketminus <yasmine.djadji@student.ehb.be>
2023-10-02 10:27:54 +0200

    fixed e3

--- e3_opteller.mjs
+++ e3_opteller.mjs
@@ -5,9 +5,11 @@ const userInput = readline.createInterface({input, output});
 // Einde van userInput voorbereiden
 
 
+let getal1 = parseFloat (await userInput.question ('Geef het eerste getal: '));
+let getal2 = parseFloat (await userInput.question ('Geef het tweede getal: '));
 
+let som = getal1 + getal2
 
-
-
+console.log ('De som is ' + som);
 
 process.exit();
