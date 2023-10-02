8ca1f2450d8587e3c65507cf7824570cca5abd78
jacketminus <yasmine.djadji@student.ehb.be>
2023-10-02 10:07:46 +0200

    fixed e1 code

--- e1_beginnen.mjs
+++ e1_beginnen.mjs
@@ -1,5 +1,8 @@
 // Voeg hier je eigen code in
 
+let name = 'Yasmine'
+let age = 19
+let favoriteGame = 'Stardew Valley'
 
 
 console.log('Hallo '+ name +', ik hoor dat je '+ age +' jaar oud bent en dat je lievelingsgame '+ favoriteGame +' is.');
